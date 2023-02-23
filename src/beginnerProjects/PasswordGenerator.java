055ab5dbcd4af9a7758b3cf9e6dd8b356b4933a9
Shanil Gunawardane <shanil.gunawardane@gmail.com>
2023-02-23 19:41:03 +1100

    First commit

---
+++ src/beginnerProjects/PasswordGenerator.java
@@ -0,0 +1,26 @@
+package beginnerProjects;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Creating password...");		 
+
+		int max = 90;
+		int min = 65;
+		String pass = "";
+
+		for (int i = 0; i<10; i++) {
+			double numbDoub = Math.random()* (max - min + 1) + min;
+			int numb = (int) numbDoub;
+			System.out.println("Number: " + numb );
+			char a = (char)numb;
+			pass = pass + a;
+
+		}
+	 
+
+		System.out.println(pass);
+
+	}
+
+}
